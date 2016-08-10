0cb88efb75254e52d21ad7322646ad5534a4c1bd
Mihail-K <mihail@platterz.ca>
2016-08-10 10:17:00 -0400

    Remove deprecated configuration parameter.

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module Squircle
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     ActiveModelSerializers.config.adapter = :json
   end
 end
