1bfd94aa2b137d4f52a120a11ee8bdd7fd27ab9b
Mihail-K <mihail@platterz.ca>
2016-08-10 12:31:24 -0400

    Specs for authenticated admins.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,19 +24,34 @@ RSpec.describe UsersController, type: :controller do
 
     it 'responds with 200' do
       get :index, format: :json
+
       expect(response.status).to eq 200
     end
 
-    it 'returns only visible users as JSON' do
+    it 'returns only visible users' do
       get :index, format: :json
+
+      expect(response.status).to eq 200
       expect(json).to have_key :users
       expect(users.count).to eq 1
       expect(users.first).to have_key :id
       expect(users.first[:id]).to eq active_user.id
     end
 
+    it 'returns all visible for authenticated admins' do
+      active_user.update admin: true
+      get :index, format: :json, params: { access_token: token.token }
+
+      expect(response.status).to eq 200
+      expect(users.count).to eq 2
+      expect(users.map { |user| user[:id] }).to contain_exactly active_user.id, deleted_user.id
+    end
+
     it %(doesn't return personal fields in the JSON) do
       get :index, format: :json
+
+      expect(response.status).to eq 200
+      expect(users.count).to eq 1
       users.each do |user|
         %i(password_digest email email_confirmed_at first_name last_name date_of_birth).each do |field|
           expect(user).not_to have_key field
@@ -48,6 +63,7 @@ RSpec.describe UsersController, type: :controller do
       create :user # Create a second user (which is not authenticated)
       get :index, format: :json, params: { access_token: token.token }
 
+      expect(response.status).to eq 200
       expect(users.count).to eq 2
       users.each do |user|
         expect(user).not_to have_key :password_digest
@@ -59,9 +75,11 @@ RSpec.describe UsersController, type: :controller do
 
     it 'always returns personal fields for authenticated administrators' do
       active_user.update admin: true
+      create :user # Create a second user (which is not authenticated)
       get :index, format: :json, params: { access_token: token.token }
 
-      expect(users.count).to eq 2
+      expect(response.status).to eq 200
+      expect(users.count).to eq 3
       users.each do |user|
         expect(user).not_to have_key :password_digest
         %i(email email_confirmed_at first_name last_name date_of_birth).each do |field|
