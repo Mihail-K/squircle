d5a4f7b907e96b90b8268c4327c1c7848093c3cf
Mihail-K <mihail@platterz.ca>
2016-08-16 11:18:23 -0400

    Clean up conversation associations.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -25,17 +25,17 @@ class Conversation < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   belongs_to :locked_by, class_name: 'User'
 
-  has_many :posts, -> { visible }, inverse_of: :conversation
+  has_many :posts, inverse_of: :conversation
 
-  has_many :post_authors, -> { visible.distinct }, through: :posts,
-                                                   source: :author,
-                                                   class_name: 'User'
-  has_many :post_characters, -> { visible.distinct }, through: :posts,
-                                                      source: :character,
-                                                      class_name: 'Character'
+  has_many :post_authors, -> { distinct }, through: :posts,
+                                           source: :author,
+                                           class_name: 'User'
+  has_many :post_characters, -> { distinct }, through: :posts,
+                                              source: :character,
+                                              class_name: 'Character'
 
-  has_one :first_post, -> { visible.order(created_at: :asc) }, class_name: 'Post'
-  has_one :last_post, -> { visible.order(created_at: :desc) }, class_name: 'Post'
+  has_one :first_post, -> { first_posts }, class_name: 'Post'
+  has_one :last_post, -> { last_posts }, class_name: 'Post'
 
   accepts_nested_attributes_for :first_post, reject_if: :all_blank
 
@@ -46,9 +46,7 @@ class Conversation < ActiveRecord::Base
 
   validate :locking_user_is_admin, if: -> { locked_changed? to: true }
 
-  before_validation :set_conversation_in_first_post, on: :create, if: :first_post
-
-  before_validation :set_author_in_first_post, on: :create, unless: :author
+  before_validation :set_first_post_author, on: :create, if: 'author.present?'
   before_validation :set_title_from_first_post, on: :create, unless: :title?
 
   before_save if: -> { locked_changed? to: true } do
@@ -58,16 +56,13 @@ class Conversation < ActiveRecord::Base
   scope :locked,   -> { where locked: true }
   scope :unlocked, -> { where locked: false }
   scope :visible,  -> { where deleted: false }
+  scope :deleted,  -> { where deleted: true }
 
   def locking_user_is_admin
     errors.add :locked, 'can only be changed by admins' unless locked_by.try(:admin?)
   end
 
-  def set_conversation_in_first_post
-    first_post.conversation = self
-  end
-
-  def set_author_in_first_post
+  def set_first_post_author
     first_post.author = author
   end
 
