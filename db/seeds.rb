76ed7cb8eeafaed10729c6ead5a697a9f9706f0f
Mihail-K <mihail@platterz.ca>
2016-09-07 15:05:33 -0400

    Seed test database before run.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Doorkeeper::Application.find_or_create_by!(
-  name: 'default', redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-  uid: '30ac6b5b2a676be9301471ffce4bf1c331c0eb28921f7d1269baea1a9ad6410a',
-  secret: 'fb6bd09d8919b6c2d8fce0dd393a55cd568510fb39f0c14e9e31445ccc860393'
+  name:         'default',
+  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+  uid:          '30ac6b5b2a676be9301471ffce4bf1c331c0eb28921f7d1269baea1a9ad6410a',
+  secret:       'fb6bd09d8919b6c2d8fce0dd393a55cd568510fb39f0c14e9e31445ccc860393'
 )
 
-unless User.exists?
-  User.create! email: 'admin@squircle.ca',
-               display_name: 'admin',
-               password: 'admin1234',
-               password_confirmation: 'admin1234',
-               date_of_birth: 20.years.ago,
-               admin: true
-end
+unless Rails.env.test?
+  unless User.exists?
+    User.create! email: 'admin@squircle.ca',
+                 display_name: 'admin',
+                 password: 'admin1234',
+                 password_confirmation: 'admin1234',
+                 date_of_birth: 20.years.ago,
+                 admin: true
+  end
 
-unless Section.exists?
-  Section.create! title: 'default',
-                  description: 'The default forum section',
-                  deleted: false,
-                  creator: User.first
+  unless Section.exists?
+    Section.create! title: 'default',
+                    description: 'The default forum section',
+                    deleted: false,
+                    creator: User.first
+  end
 end
