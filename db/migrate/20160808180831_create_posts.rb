3a707380bd82759872e27aa301ca79cdc50af631
Mihail-K <mihail@platterz.ca>
2016-10-06 19:18:19 -0400

    Quick fixes.

--- db/migrate/20160808180831_create_posts.rb
+++ db/migrate/20160808180831_create_posts.rb
@@ -3,9 +3,9 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string     :title
       t.text       :body, null: false
-      t.references :poster,    foreign_key: true, index: true, null: false
-      t.references :editor,    foreign_key: true, index: true
-      t.references :character, foreign_key: true, index: true
+      t.references :poster,    foreign_key: true, index: true, null: false, references: :users
+      t.references :editor,    foreign_key: true, index: true, references: :users
+      t.references :character, foreign_key: true, index: true, references: :characters
       t.timestamps  null: false
     end
   end
