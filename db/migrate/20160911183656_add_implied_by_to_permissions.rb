6d77e0ef50b10a8762242c3f676c98ec206dec40
Mihail-K <mihail@platterz.ca>
2016-10-06 19:38:05 -0400

    Fix table references.

--- db/migrate/20160911183656_add_implied_by_to_permissions.rb
+++ db/migrate/20160911183656_add_implied_by_to_permissions.rb
@@ -1,5 +1,5 @@
 class AddImpliedByToPermissions < ActiveRecord::Migration[5.0]
   def change
-    add_reference :permissions, :implied_by, foreign_key: true
+    add_reference :permissions, :implied_by, foreign_key: { to_table: :permissions }, index: true
   end
 end
