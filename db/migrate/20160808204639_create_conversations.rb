db3233d48c1364e135882ba01b8830820b01ba6f
Mihail-K <mihail@platterz.ca>
2016-10-06 19:25:14 -0400

    Fix table references.

--- db/migrate/20160808204639_create_conversations.rb
+++ db/migrate/20160808204639_create_conversations.rb
@@ -4,7 +4,7 @@ class CreateConversations < ActiveRecord::Migration[5.0]
       t.string     :title, null: false
       t.integer    :posts_count, null: false, default: 0
       t.boolean    :deleted, null: false, default: false
-      t.references :author, foreign_key: true, index: true, null: false
+      t.references :author, foreign_key: { to_table: :users }, index: true, null: false
       t.timestamps  null: false
     end
   end
