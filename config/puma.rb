19191d12aa6d54fa79dd41bd544c68d4afe4abd8
Mihail-K <mihail@platterz.ca>
2016-10-06 19:53:43 -0400

    Quick fixes.

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,6 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-  config.cache_store = :redis_store, ENV['REDIS_URL']
 
   if defined?(Resque)
     Resque.redis = ENV["REDIS_URL"]
