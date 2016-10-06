4410a196f03c51fa0086cdb57809659025c4c9f7
Mihail-K <mihail@platterz.ca>
2016-10-06 19:37:02 -0400

    Fix table references.

--- db/migrate/20160908134204_create_roles.rb
+++ db/migrate/20160908134204_create_roles.rb
@@ -4,7 +4,7 @@ class CreateRoles < ActiveRecord::Migration[5.0]
       t.string     :name, null: false, index: { unique: true }
       t.text       :description
       t.boolean    :deleted, null: false, index: true, default: false
-      t.references :deleted_by, foreign_key: true, index: true, references: :users
+      t.references :deleted_by, foreign_key: { to_table: :users }, index: true, references: :users
       t.timestamp  :deleted_at
       t.timestamps null: false
     end
