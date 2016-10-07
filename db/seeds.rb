2631a8ab8da2a5afcf145f0593d616956074f382
Mihail-K <mihail@platterz.ca>
2016-10-06 20:03:08 -0400

    Fix seed order.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Dir[Rails.root.join('db/seeds/**/*.rb')].sort_by(&:to_i).each do |file_name|
+Dir[Rails.root.join('db/seeds/**/*.rb')].sort.each do |file_name|
   require file_name
 end
 
