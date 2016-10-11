27a092569c4d46677dcac0fae8dbe8d5c12ae7d4
Mihail-K <mihail@platterz.ca>
2016-10-11 10:18:56 -0400

    Actually validate schema.

--- spec/support/api_schema_matcher.rb
+++ spec/support/api_schema_matcher.rb
@@ -3,7 +3,7 @@ RSpec::Matchers.define :match_response_schema do |schema|
     schema_directory = "#{Dir.pwd}/spec/support/api/schemas"
     schema_path      = "#{schema_directory}/#{schema}.json"
 
-    @result = JSON::Validator.fully_validate(schema_path, response.body)
+    @result = JSON::Validator.fully_validate(schema_path, response.body, validate_schema: true)
     @result.blank?
   end
 
