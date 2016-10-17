cc8c97b6d9045c2f0b099c9e17ff5943c403e4e9
Mihail-K <mihail@platterz.ca>
2016-10-16 20:48:16 -0400

    Fix uploader avatar filename.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,8 +12,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.name.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -28,8 +26,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [100, 100]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
@@ -38,11 +34,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
     %r{image\/}
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
     return nil if original_filename.blank?
     require 'active_support/core_ext/digest/uuid'
-    "#{Digest::UUID.uuid_v5(model.class.name, original_filename)}.#{file.extension}"
+    "#{Digest::UUID.uuid_v5(model.class.name, model.id.to_s)}.#{file.extension}"
   end
 end
