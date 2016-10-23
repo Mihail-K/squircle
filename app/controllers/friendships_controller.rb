e3baaa4d842680ab9710c666b8045b76e139253a
Mihail-K <mihail@platterz.ca>
2016-10-23 17:13:58 -0400

    Rubocop fixes.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class FriendshipsController < ApplicationController
   before_action :doorkeeper_authorize!, except: %i(index show)
 
--- app/serializers/friendship_serializer.rb
+++ app/serializers/friendship_serializer.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class FriendshipSerializer < ActiveModel::Serializer
   attribute :id
   attribute :user_id
