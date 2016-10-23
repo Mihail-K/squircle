3f7475f86e845b73e482c2d28d6667cbc432f21c
Mihail-K <mihail@platterz.ca>
2016-10-23 18:17:10 -0400

    Add notification serializer.

---
+++ app/serializers/notification_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+class NotificationSerializer < ActiveModel::Serializer
+  attribute :id
+  attribute :user_id
+  attribute :targetable_id
+  attribute :targetable_type
+
+  attribute :title
+  attribute :read
+  attribute :dismissed
+  attribute :created_at
+  attribute :updated_at
+
+  belongs_to :user
+  belongs_to :targetable, polymorphic: true
+end
