73a24e9eb2413bb01700b8fd86578786ef1c7ef4
Mihail-K <mihail@platterz.ca>
2016-11-05 11:20:56 -0400

    Add specs for post notification creation.

--- spec/jobs/post_notification_job_spec.rb
+++ spec/jobs/post_notification_job_spec.rb
@@ -2,5 +2,35 @@
 require 'rails_helper'
 
 RSpec.describe PostNotificationJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let! :post do
+    create :post
+  end
+
+  context 'without subscribers' do
+    it "doesn't create a notification" do
+      expect do
+        PostNotificationJob.perform_now(post.id)
+      end.not_to change { Notification.count }
+    end
+  end
+
+  context 'with subscribers to the conversation' do
+    let! :subscription do
+      create :subscription, conversation: post.conversation
+    end
+
+    it 'creates a new notification' do
+      expect do
+        PostNotificationJob.perform_now(post.id)
+      end.to change { Notification.count }.by(1)
+    end
+
+    it "doesn't create a notification if one already exists" do
+      create :notification, user: subscription.user, targetable: post
+
+      expect do
+        PostNotificationJob.perform_now(post.id)
+      end.not_to change { Notification.count }
+    end
+  end
 end
