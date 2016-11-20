2ed4c36bf8756ffaaf9465be5ad34486271aa400
Mihail-K <mihail@platterz.ca>
2016-11-20 13:28:40 -0500

    Rubocop fixes.

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -12,13 +12,13 @@ private
 
   def set_indices
     @indices = Index.search(query: {
-      multi_match: {
-        query:  params[:query] || '',
-        fields: %w(primary   primary.english^1.5
-                   secondary secondary.english^1.5
-                   tertiary  tertiary.english^1.5)
-      }
-    })
+                              multi_match: {
+                                query:  params[:query] || '',
+                                fields: %w(primary primary.english^1.5
+                                           secondary secondary.english^1.5
+                                           tertiary tertiary.english^1.5)
+                              }
+                            })
     @indices = @indices.page(params[:page]).per(params[:count])
     @indices = @indices.records
   end
