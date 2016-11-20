ca53241f367c015e996a8620e4d88cae3d8e8768
Mihail-K <mihail@platterz.ca>
2016-11-20 12:24:45 -0500

    Fix query boosting.

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -14,9 +14,9 @@ private
     @indices = Index.search(query: {
       multi_match: {
         query:  params[:query],
-        fields: %w(primary   primary.english^2   primary.raw^3
-                   secondary secondary.english^2 secondary.raw^3
-                   tertiary  tertiary.english^2  tertiary.raw^3)
+        fields: %w(primary   primary.english^1.5
+                   secondary secondary.english^1.5
+                   tertiary  tertiary.english^1.5)
       }
     })
     @indices = @indices.page(params[:page]).per(params[:count])
