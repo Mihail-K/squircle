28b27264b5b6136f96dd62371770198e25f7a8e4
Mihail-K <mihail@platterz.ca>
2016-12-05 09:53:10 -0500

    Remove unnecessary methods from policy.

--- app/policies/password_reset_policy.rb
+++ app/policies/password_reset_policy.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 class PasswordResetPolicy < ApplicationPolicy
-  alias password_reset record
-
-  def show?
-    scope.exists?(token: password_reset)
-  end
-
-  def create?
-    true
-  end
-
-  def update?
-    true
-  end
-
   def permitted_attributes_for_create
     :email
   end
